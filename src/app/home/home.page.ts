07cac327ddbb440081ed8b0de9339fec5bb0e690
Christo <christo.mabbs@gmail>
2025-02-12 15:07:01 +1030

    updates

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -87,6 +87,7 @@ export class HomePage {
   displayVariance: number = 10;
   displayPromptsLength: number = 3;
   displayPrompts: Array<{
+    phase: number,
     category: string,
     prompt: string,
   }> = [];
@@ -99,21 +100,37 @@ export class HomePage {
 
     // Load the initial prompts (number of prompts = displayPromptsLength)
     for (let i = 0; i < this.displayPromptsLength; i++) {
+      await new Promise(resolve => setTimeout(resolve, 500));
       this.addRandomPrompt();
     }
 
   }
 
-  hidePrompt(index: number) {
+  async hidePrompt(index: number) {
 
+    // Make the prompt phase 2
+    this.displayPrompts[index].phase = 2;
+
+    // wait 500ms
+    await new Promise(resolve => setTimeout(resolve, 500));
+
+    // Make the prompt phase 3
+    this.displayPrompts[index].phase = 3;
+
+    // Wait 500ms
+    await new Promise(resolve => setTimeout(resolve, 500));
+
+    // Remove the prompt from the displayPrompts array
     this.displayPrompts.splice(index, 1);
 
-    // Add a new prompt to replace the one that was removed
-    this.addRandomPrompt();
+    // Add a new prompt
+    this.addRandomPrompt( );
 
   }
 
   addRandomPrompt() {
+
+    // Get the categories that have prompts that haven't been displayed yet
     const availableCategories = Object.keys(this.prompts)
       .filter(category =>
         this.prompts[category].some(prompt =>
@@ -121,28 +138,41 @@ export class HomePage {
         )
       );
 
+    // If there are no new prompts available, log a warning and return
     if (availableCategories.length === 0) {
       console.warn("No new unique prompts available.");
       return;
     }
 
+    // Choose a random category from the available categories
     const randomCategory = availableCategories[Math.floor(Math.random() * availableCategories.length)];
     const availablePrompts = this.prompts[randomCategory].filter(
       prompt => !this.displayPrompts.some(dp => dp.prompt === prompt)
     );
 
+    // If there are no new prompts available, log a warning and return
     if (availablePrompts.length === 0) {
       console.warn(`No new unique prompts available in category: ${randomCategory}`);
       return;
     }
 
+    // Choose a random prompt from the available prompts
     const randomPrompt = availablePrompts[Math.floor(Math.random() * availablePrompts.length)];
 
-    this.displayPrompts.push({
+    // Create a new prompt object
+    let newPrompt = {
+      phase: 0,
       category: randomCategory,
       prompt: randomPrompt
-    });
-  }
+    };
 
+    this.displayPrompts.push(newPrompt);
+
+    // in 100 ms set the phase to 1
+    setTimeout(() => {
+      newPrompt.phase = 1;
+    }, 100);
+
+  }
 
 }
